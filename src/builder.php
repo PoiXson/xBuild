1735b37fb54cd667651e01416b377ba4b60ade1e
lorenzop <lorenzo@poixson.com>
2016-03-04 08:54:32 -0500

    added xbuild.php and builder.php files

---
+++ src/builder.php
@@ -0,0 +1,116 @@
+<?php
+/*
+ * PoiXson xBuild - Build and deploy tools
+ *
+ * @copyright 2015-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuild;
+
+use pxn\phpUtils\San;
+
+
+class builder {
+
+	public $config;
+	public $BuildNumber = NULL;
+	public $goals       = array();
+
+
+
+	public function __construct($config) {
+		$this->config = $config;
+	}
+
+
+
+	public function LoadGoals($goals) {
+		if (!\is_array($goals)) {
+			throw new \Exception('Invalid goals argument provided!');
+		}
+		$count = 0;
+		foreach ($goals as $goalName) {
+			// goal config
+			$goalConfig = $this->config->getGoalConfig($goalName);
+			if ($goalConfig === NULL) {
+				echo 'Goal not configured for this project: '.$goalName."\n";
+				exit(1);
+			}
+			// load goal object
+			$result = $this->LoadGoal(
+				$goalName,
+				$goalConfig
+			);
+			if($result !== TRUE) {
+				echo "Failed to load goal: {$goalName}\n";
+				exit(1);
+			}
+			$count++;
+		}
+		echo "Loaded {$count} goals\n";
+		return TRUE;
+	}
+	public function LoadGoal($goalName, $config) {
+		$goalName = San::AlphaNum($goalName);
+		$file = __DIR__.'/goals/goal_'.$goalName.'.php';
+		// ensure goal exists
+		if (!\file_exists($file)) {
+			echo 'Goal type not found: '.$goalName."\n";
+			exit(1);
+		}
+		// load goal class object
+		$clss = '\\pxn\\xBuild\\goals\\goal_'.$goalName;
+		$goal = new $clss(
+			$this,
+			$config
+		);
+		$this->goals[] = $goal;
+		return TRUE;
+	}
+
+
+
+	public function run() {
+		$goalsString = '';
+		$first = TRUE;
+		foreach ($this->goals as $goal) {
+			if ($first) {
+				$first = FALSE;
+			} else {
+				$goalsString .= ', ';
+			}
+			$goalsString .= $goal->getName();
+		}
+		echo "\n\n";
+		echo "RUNNING...  [ {$goalsString} ]\n";
+		echo "\n\n";
+		// run goals
+		$first = TRUE;
+		foreach ($this->goals as $goal) {
+			if ($first) {
+				$first = FALSE;
+				echo "----------------------------------------\n";
+				echo "\n\n";
+			}
+			$goal->run();
+			echo "\n\n";
+			echo "----------------------------------------\n";
+			echo "\n\n";
+		}
+	}
+
+
+
+	// build number
+	public function getBuildNumber() {
+		if(empty($this->BuildNumber)) {
+			return '<Not Set>';
+		}
+		return (string) ((int) $this->BuildNumber);
+	}
+
+
+
+}
---
+++ src/xbuild.php
@@ -0,0 +1,217 @@
+#!/usr/bin/php
+<?php
+/*
+ * PoiXson xBuild - Build and deploy tools
+ *
+ * @copyright 2015-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuild;
+
+use pxn\xBuild\configs\config_xbuild;
+use pxn\xBuild\configs\config_xdeploy;
+
+use pxn\phpUtils\Strings;
+use pxn\phpUtils\Config;
+
+
+
+// defines
+const BUILD_CONFIG_FILE  = 'xbuild.json';
+const DEPLOY_CONFIG_FILE = 'xdeploy.json';
+
+
+
+// find autoloader.php
+{
+	$SearchPaths = [
+		__DIR__,
+		__DIR__.'/..',
+	];
+	$found = FALSE;
+	foreach ($SearchPaths as $path) {
+		if (empty($path)) continue;
+		$path = \realpath($path.'/vendor/');
+		if (empty($path)) continue;
+		if (\file_exists($path.'/autoload.php')) {
+			require($path.'/autoload.php');
+			$found = TRUE;
+			break;
+		}
+	}
+	if (!$found) {
+		echo "Failed to find composer autoload.php !\n";
+		exit(1);
+	}
+}
+
+
+
+// check os
+\pxn\phpUtils\System::RequireLinux();
+
+// require shell
+if (!Config::isShell()) {
+	echo "This script must be run locally as a shell script!\n";
+	exit(1);
+}
+
+
+
+function DisplayLogo() {
+	global $NoLogo;
+	if ($NoLogo !== FALSE)
+		return;
+	echo "\n";
+	echo ' ██╗  ██╗██████╗ ██╗   ██╗██╗██╗     ██████╗ '."\n";
+	echo ' ╚██╗██╔╝██╔══██╗██║   ██║██║██║     ██╔══██╗'."\n";
+	echo '  ╚███╔╝ ██████╔╝██║   ██║██║██║     ██║  ██║'."\n";
+	echo '  ██╔██╗ ██╔══██╗██║   ██║██║██║     ██║  ██║'."\n";
+	echo ' ██╔╝ ██╗██████╔╝╚██████╔╝██║███████╗██████╔╝'."\n";
+	echo ' ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝ '."\n";
+	echo "\n";
+}
+
+
+
+function DisplayHelp() {
+	global $argv;
+	echo "Usage:\n";
+	echo '  '.$argv[0].' [options] [goals]'."\n";
+	echo "\n";
+	echo "Goals:\n";
+	echo "  clean\n";
+	echo "  build\n";
+	echo "  deploy\n";
+	echo "\n";
+	echo "Options:\n";
+	echo "  -b, --build-number=n       Sets the build number\n";
+	echo "  -D, --deploy-config-depth  Number of parent directories to ascend\n";
+	echo "                             when searching for ".DEPLOY_CONFIG_FILE." config file\n";
+	echo "\n";
+	echo "  -w, --max-wait             Max wait time in seconds when another instance is busy\n";
+	echo "                             set to -1 for no timeout, or 0 to fail immediately\n";
+	echo "                             default: 300 (5 minutes)\n";
+	echo "\n";
+	echo "  -d, --debug                Debug mode, most verbose logging\n";
+	echo "  -t, --dry                  Dry run, without writing files\n";
+	echo "\n";
+	echo "  -h, --help                 Display this help message\n";
+	echo "  --no-logo                  Disables the startup logo\n";
+	echo "\n";
+	exit(0);
+}
+
+
+
+$NoLogo = FALSE;
+$BuildNumber = NULL;
+$GoalArgs = array();
+for ($i=1; $i<count($argv); $i++) {
+	switch ($argv[$i]) {
+	case '-b':
+	case '--build-number':
+		$i++;
+		if (Numbers::isNumber($argv[$i])) {
+			$BuildNumber = (int) $argv[$i];
+		} else {
+			$BuildNumber = NULL;
+		}
+		break;
+	case '-D':
+	case '--deploy-config-depth':
+		
+		break;
+	case '-w':
+	case '--max-wait':
+		
+		break;
+	case '-d':
+	case '--debug':
+		
+		break;
+	case '-t':
+	case '--dry':
+		
+		break;
+	case '-h':
+	case '--help':
+		DisplayLogo();
+		DisplayHelp();
+		exit(0);
+	case '--no-logo':
+		$NoLogo = TRUE;
+		break;
+	default:
+		if (Strings::StartsWith($argv[$i], '--')) {
+			echo 'Unknown argument: '.$argv[$i]."\n";
+			exit(1);
+		}
+		$GoalArgs[] = $argv[$i];
+		break;
+	}
+}
+
+
+
+DisplayLogo();
+echo "\n\n";
+
+
+
+// load build config
+$config = NULL;
+{
+	$file = BUILD_CONFIG_FILE;
+	if (! \file_exists($file)) {
+		echo "Config file not found: {$file}\n";
+		exit(1);
+	}
+	$config = new config_xbuild();
+	if ( ! $config->LoadFile($file) ) {
+		echo "Failed to load config file: {$file}\n";
+		exit(1);
+	}
+}
+// load deploy config
+$deploy = NULL;
+{
+//	$file = DEPLOY_CONFIG_FILE;
+//	if (\file_exists($file)) {
+//		$deploy = new config_xbuild();
+//		if ( ! $deploy->LoadFile($file) ) {
+//			echo "Failed to load config file: {$file}\n";
+//			exit(1);
+//		}
+//	}
+}
+
+
+
+// load builder
+$builder = new builder(
+	$config
+);
+$builder->BuildNumber = $BuildNumber;
+
+
+
+// load goals
+if(count($GoalArgs) == 0) {
+	$defaultGoals = $config->getDefaultGoals();
+	$builder->LoadGoals($defaultGoals);
+} else {
+	$builder->LoadGoals($GoalArgs);
+}
+
+
+
+// run goals
+$builder->run();
+
+
+
+echo "\nFinished!\n\n";
+exit(0);
