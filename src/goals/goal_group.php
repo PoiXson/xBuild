3159b802564a429482ffc59320010259c565b9ff
lorenzop <lorenzo@poixson.com>
2016-03-16 19:56:05 -0400

    added goal_group class

---
+++ src/goals/goal_group.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * PoiXson xBuild - Build and deploy tools
+ *
+ * @copyright 2015-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuild\goals;
+
+
+class goal_group extends Goal {
+
+
+
+	public function getName() {
+		return 'group';
+	}
+
+
+
+	public function run() {
+		$this->title('Running group..');
+		echo "\n";
+fail ('Sorry, this goal is unfinished!');
+	}
+
+
+}
