c012fe81f189f674e4c9d5868038f1f077d35424
lorenzop <lorenzo@poixson.com>
2016-03-27 21:49:32 -0400

    changed composer goal to use GoalShell trait

--- src/goals/goal_composer.php
+++ src/goals/goal_composer.php
@@ -11,31 +11,31 @@ namespace pxn\xBuild\goals;
 
 
 class goal_composer extends Goal {
+	use GoalShell;
 
 
 
 	public function getType() {
 		return 'composer';
 	}
+	public function displayTitle() {
+		$name   = $this->getName();
+		$prefix = $this->getTitlePrefix();
+		self::title("{$prefix} {$name}..");
+	}
 
 
 
+	// Process Exit Codes#
+	//  0: OK
+	//  1: Generic/unknown error code
+	//  2: Dependency solving error code
 	public function run() {
-fail ('Sorry, this goal is unfinished!');
-
-
-
-// composer install --profile -o
+		return $this->runShellHex($this->args);
+//		$args = $this->args['Args'];
+//		$cmd = "composer {$args}";
+//		return $this->runShell($cmd);
 // composer show -t --profile
-
-//Process Exit Codes#
-//0: OK
-//1: Generic/unknown error code
-//2: Dependency solving error code
-
-
-
-
 	}
 
 
