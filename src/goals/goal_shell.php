3fd896ce03146a3ee7d1c8099d69fe3c3c079aaf
lorenzop <lorenzo@poixson.com>
2016-03-28 03:42:31 -0400

    added shell goal

---
+++ src/goals/goal_shell.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * PoiXson xBuild - Build and deploy tools
+ *
+ * @copyright 2015-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuild\goals;
+
+
+class goal_shell extends Goal {
+	use GoalShell;
+
+
+
+	public function getType() {
+		return 'shell';
+	}
+
+
+
+	public function run() {
+		return $this->runShellHex($this->args);
+	}
+
+
+
+}
