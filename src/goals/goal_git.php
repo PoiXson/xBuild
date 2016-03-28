ecd8dc395b6518926fec4631aa20f171a4369688
lorenzop <lorenzo@poixson.com>
2016-03-28 03:44:43 -0400

    added git goal extending shell goal

---
+++ src/goals/goal_git.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * PoiXson xBuild - Build and deploy tools
+ *
+ * @copyright 2015-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuild\goals;
+
+
+class goal_git extends goal_shell {
+
+
+
+	public function getType() {
+		return 'git';
+	}
+
+
+
+}
