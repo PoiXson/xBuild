568fedbc21ec78f0cc12b91961384923b74a26af
lorenzop <lorenzo@poixson.com>
2016-03-28 21:13:00 -0400

    moved getGoals() and loadGoals() functions from config_xbuild to config_goals class

---
+++ src/configs/config_goals.php
@@ -0,0 +1,85 @@
+<?php
+/*
+ * PoiXson xBuild - Build and deploy tools
+ *
+ * @copyright 2015-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\xBuild\configs;
+
+use pxn\phpUtils\San;
+use pxn\xBuild\goals\Goal;
+
+
+trait config_goals {
+
+	public $goals = NULL;
+
+
+
+	/**
+	 * Get the goals
+	 * @return array[goal] or null on failure
+	 */
+	public function getGoals() {
+		if (!\is_array($this->goals)) {
+			$this->loadGoals();
+		}
+		if (!isset($this->json['Goals']))
+			return NULL;
+		return $this->goals;
+	}
+	public function loadGoals() {
+		if (\is_array($this->goals))
+			return;
+		$goals = array();
+		$count = 0;
+		foreach ($this->json['Goals'] as $name => $args) {
+			// validate args array
+			if (!\is_array($args)) {
+				fail ('Invalid config structure! goal arguments should be a map!');
+				exit(1);
+			}
+			// validate name value
+			if (empty($name)) {
+				fail ('Empty goal name found in config!');
+				exit(1);
+			}
+			if (!San::Validate_AlphaNumSafe($name)) {
+				fail ("Invalid goal name found in config! {$name}");
+				exit(1);
+			}
+			// default type value
+			$type = isset($args['Type'])
+					? $args['Type']
+					: $name;
+			unset($args['Type']);
+			// validate type value
+			if (!San::Validate_AlphaNumSafe($type)) {
+				fail ("Invalid goal type found in config! {$type}");
+				exit(1);
+			}
+			// load goal class
+			{
+				$g = Goal::LoadGoal(
+					$name,
+					$type,
+					$args
+				);
+				if ($g == NULL) {
+					fail ("Failed to load goal: {$name}");
+					exit(1);
+				}
+				$goals[$name] = $g;
+			}
+			$count++;
+		}
+		$this->goals = $goals;
+		return $goals;
+	}
+
+
+
+}
--- src/configs/config_xbuild.php
+++ src/configs/config_xbuild.php
@@ -14,6 +14,7 @@ use pxn\xBuild\goals\Goal;
 
 
 class config_xbuild extends config_abstract {
+	use config_goals;
 
 	const KEY_NAME          = 'Name';
 	const KEY_VERSION       = 'Version';
@@ -69,66 +70,14 @@ class config_xbuild extends config_abstract {
 
 
 
-	/**
-	 * Get the project goals
-	 * @return array[goal] or null on failure
-	 */
 	public function getGoals() {
-		if (!\is_array($this->goals)) {
-			$this->loadGoals();
-		}
-		if (!isset($this->json[self::KEY_GOALS]))
-			return NULL;
-		return $this->goals;
-	}
-	public function loadGoals() {
-		if (\is_array($this->goals))
-			return;
-			$goals = array();
-			$count = 0;
-			foreach ($this->json[self::KEY_GOALS] as $name => $args) {
-				// validate args array
-				if (!\is_array($args)) {
-					fail ('Invalid config structure! goal arguments should be a map!');
-					exit(1);
-				}
-				// validate name value
-				if (empty($name)) {
-					fail ('Empty goal name found in config!');
-					exit(1);
-				}
-				if (!San::Validate_AlphaNumSafe($name)) {
-					fail ("Invalid goal name found in config! {$name}");
-					exit(1);
-				}
-				// default type value
-				$type = isset($args['Type'])
-				? $args['Type']
-				: $name;
-				unset($args['Type']);
-				// validate type value
-				if (!San::Validate_AlphaNumSafe($type)) {
-					fail ("Invalid goal type found in config! {$type}");
-					exit(1);
-				}
-				// load goal class
-				{
-					$g = Goal::LoadGoal(
-							$name,
-							$type,
-							$args
-							);
-					if ($g == NULL) {
-						fail ("Failed to load goal: {$name}");
-						exit(1);
-					}
-					$goals[$name] = $g;
-				}
-				$count++;
+		$goals = $this->goals;
+		foreach($this->configGlobal->goals as $key => $val) {
+			if (!isset($goals[$key])) {
+				$goals[$key] = $val;
 			}
-			echo "Found [ {$count} ] goals in config\n";
-			$this->goals = $goals;
-			return $goals;
+		}
+		return $goals;
 	}
 
 
