8bd415ef5e377c41f47bfaee4139c433b04d77df
lorenzop <lorenzo@poixson.com>
2016-07-08 14:18:39 -0400

    added --restore --post-install and --post-update flags to symlinks.sh script

--- symlinks.sh
+++ symlinks.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
+
 echo
-echo 'Creating symlinks..'
+if [[ "$1" == "--restore" ]] || [[ "$1" == "--pre" ]]; then
 
-ln -svf vendor/pxn/phputils/pxnloader.php pxnloader.php
 
+	echo 'Restoring vendor..'
+
+	if [ -L "vendor/pxn/phputils" ]; then
+		pushd "vendor/pxn" || exit 1
+			rm -fv phputils || exit 1
+			[[ -d phputils.original ]] && \
+				mv -v phputils.original phputils || exit 1
+		popd
+	fi
+
+
+else
+
+
+	echo 'Creating symlinks..'
+
+	ln -svf vendor/pxn/phputils/pxnloader.php pxnloader.php
+
+	if [ -d "../phpUtils/" ]; then
+		echo
+		echo ' *** Found local phpUtils..'
+		pushd "vendor/pxn/" || exit 1
+			[[ ! -L phputils ]] && \
+				mv -v phputils phputils.original
+			ln -svf ../../../phpUtils/ phputils
+		popd
+	fi
+
+
+fi
 echo
